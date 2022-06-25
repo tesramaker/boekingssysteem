9c56272468fa77789da855e8801b4104c6f84e7e
tesramaker <tessa.ramaker@student.nhlstenden.com>
2022-06-25 11:08:50 +0200

    disable/enable location

--- Boekingssysteem/Boekingssysteem/MainPage.xaml.cs
+++ Boekingssysteem/Boekingssysteem/MainPage.xaml.cs
@@ -5,7 +5,15 @@ public partial class MainPage : ContentPage
 	public MainPage()
 	{
 		InitializeComponent();
-	}
+		CheckBox checkbox = FindByName("randomLocation") as CheckBox;
+		Entry location = FindByName("location") as Entry;
+		var listener = new TapGestureRecognizer();
+        listener.Tapped += ( sender, e ) =>
+        {
+            location.IsEnabled = !checkbox.IsChecked;
+        };
+        checkbox.GestureRecognizers.Add ( listener );
+        }
 
 	async void OnSearchButtonClicked ( object sender, EventArgs e)
 	{
