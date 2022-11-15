d4a6f9db0cbf7dc3e1a0de6957d346d265935e24
tesramaker <tessa.ramaker@student.nhlstenden.com>
2022-11-15 21:02:05 +0100

    update location picker

--- Boekingssysteem/Boekingssysteem/MainPage.xaml.cs
+++ Boekingssysteem/Boekingssysteem/MainPage.xaml.cs
@@ -7,8 +7,8 @@ public partial class MainPage : ContentPage
     public MainPage ( )
     {
         InitializeComponent ( );
-        Layout ();
-    }
+        Layout ( );
+        }
 
     async void Layout()
     {
