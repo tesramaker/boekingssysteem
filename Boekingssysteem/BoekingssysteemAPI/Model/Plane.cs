cf2423c801dd83dc0260bd2f7e74ce49a3476482
Avijn <arjan.vijn@student.nhlstenden.com>
2022-11-17 21:11:12 +0100

    Removed commented code / Fixed some other issue with nullable fields

--- Boekingssysteem/BoekingssysteemAPI/Model/Plane.cs
+++ Boekingssysteem/BoekingssysteemAPI/Model/Plane.cs
@@ -8,31 +8,7 @@ namespace BoekingssysteemAPI.Model
         public int id { get; set; }
         public string name { get; set; }
         public int seats { get; set; }
-        public string airline { get; set; }
-        public int defaultAmountSeats { get; set; }
-
-        //public Plane(int id, string name, int seats, string airline, int defaultAmountSeats)
-        //{
-        //    this.id = id;
-        //    this.name = name;
-        //    this.seats = seats;
-        //    this.airline = airline;
-        //    this.defaultAmountSeats = defaultAmountSeats;
-        //}
-
-        //public Plane(int id,string name, int seats, string airline)
-        //{
-        //    this.id = id;
-        //    this.name = name;
-        //    this.seats = seats;
-        //    this.airline = airline;
-        //}
-
-        //public Plane(string name, int seats, string airline)
-        //{
-        //    this.name = name;
-        //    this.seats = seats;
-        //    this.airline = airline;
-        //}
+        public string? airline { get; set; }
+        public int? defaultAmountSeats { get; set; }
     }
 }
--- Boekingssysteem/BoekingssysteemAPI/Model/User.cs
+++ Boekingssysteem/BoekingssysteemAPI/Model/User.cs
@@ -5,51 +5,10 @@
         public int id { get; set; }
         public string name { get; set; }
         public string password { get; set; }
-        public DateTime lastLoginDate { get; set; }
-        public Guid loginToken { get; set; }
-        public bool cancellationInsurance { get; set; }
+        public DateTime? lastLoginDate { get; set; }
+        public Guid? loginToken { get; set; }
+        public bool? cancellationInsurance { get; set; }
         //TODO AccessLevel not yet implemented, perhaps for future version (customer, company, admin etc), not yet implemented in the DB
         //public int accessLevel { get; set; }
-
-        //public User(int id, string name, string password, DateTime lastLoginDate, Guid loginToken, bool cancellationInsurance)
-        //{
-        //    this.id = id;
-        //    this.name = name;
-        //    this.password = password;
-        //    this.lastLoginDate = lastLoginDate;
-        //    this.loginToken = loginToken;
-        //    this.cancellationInsurance = cancellationInsurance;
-        //}
-        //public User(int id, string name, string password, DateTime lastLoginDate, Guid loginToken)
-        //{
-        //    this.id = id;
-        //    this.name = name;
-        //    this.password = password;
-        //    this.lastLoginDate = lastLoginDate;
-        //    this.loginToken = loginToken;
-        //}
-
-        //public User(int id, string name, string password)
-        //{
-        //    this.id = id;
-        //    this.name = name;
-        //    this.password = password;
-        //}
-        //public User(string name, string password)
-        //{
-        //    this.name = name;
-        //    this.password = password;
-        //}
-
-        //public User(int id, Guid logoinToken)
-        //{
-        //    this.id = id;
-        //    this.loginToken = loginToken;
-        //}
-
-        //public User()
-        //{
-        //    this.loginToken = Guid.Empty;
-        //}
     }
 }
